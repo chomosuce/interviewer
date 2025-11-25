b7d24a53a4b14b6d05babc9f49aa571727cfd996
Igor0006 <igordubrovskij69@gmail.com>
2025-11-25 23:33:56 +0300

    chore: test prompts

---
+++ src/main/java/com/t1impulse/interviewer/config/TestTopic.java
@@ -0,0 +1,15 @@
+package com.t1impulse.interviewer.config;
+
+public enum TestTopic {
+    MACHINE_LEARNING,
+    BACKEND,
+    FRONTEND,
+    ALGORITHMS,
+    DATA_SCIENCE,
+    DEVOPS,
+    OOP,
+    SQL,
+    NETWORKS,
+    INFOSEC,
+    CPP
+}
